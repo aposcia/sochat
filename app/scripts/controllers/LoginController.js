10c684d8a4005a238dd38ceea52afa6b4ff85248
Diego Castronuovo <castronu>
2015-10-03 12:52:38 +0200

    Registration working

---
+++ app/scripts/controllers/LoginController.js
@@ -0,0 +1,13 @@
+/*global Firebase*/
+(function(angular) {
+  'use strict';
+
+  angular.module('Sochat')
+    .controller('LoginController', function($scope, FBURL, $window, $firebase, $location, $firebaseAuth) {
+
+
+
+
+    });
+
+}(window.angular));
